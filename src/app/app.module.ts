bcd8770d06ce9140e7ec87b9c47d9731b99f5b50
aanSingh <singh.aan909@gmail.com>
2019-08-27 17:15:09 +0530

    3 files modified, 4 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RoutingModule} from './routing.module';
 
 import { AppComponent } from './app.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
@@ -8,10 +9,10 @@ import { CompanyInfoComponent } from './components/company-info/company-info.com
 import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 import { PostDescriptionComponent } from './components/post-description/post-description.component';
 import { PostsWrapperComponent } from './components/posts-wrapper/posts-wrapper.component';
-import { PostComponent } from './components/post/post.component';
+import { PostComponent } from './components/posts-wrapper/post/post.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, RoutingModule ],
   declarations: [ AppComponent, SideNavComponent, CompanyInfoComponent, WelcomePageComponent, PostDescriptionComponent, PostsWrapperComponent, PostComponent ],
   bootstrap:    [ AppComponent ]
 })
